259891503a4228f4ad66eaca53ce7ea7b3aa9457
sdkevorkian <sdkevorkian@gmail.com>
2017-05-09 21:25:47 -0700

    did the MVP

--- js/app.js
+++ js/app.js
@@ -8,40 +8,75 @@ fruitsAndVeggies.controller('Main', ['$scope', function($scope) {
     $scope.veggies = vegetables;
     $scope.userFruits = [];
     $scope.userVeggies = [];
-
+    $scope.gameEnd = false;
+    $scope.gameWon = false;
 
     $scope.toFruits = function(i) {
         var thisFruit = $scope.allProduce[i];
         $scope.userFruits.push(thisFruit);
         $scope.allProduce.splice(i, 1);
-
+        checkIfOver();
     };
 
     $scope.toVeggies = function(i) {
         $scope.userVeggies.push($scope.allProduce[i]);
         $scope.allProduce.splice(i, 1);
+        checkIfOver();
     };
 
     $scope.toRight = function(i) {
         $scope.allProduce.push($scope.userFruits[i]);
         $scope.userFruits.splice(i, 1);
+        checkIfOver();
     };
 
 
     $scope.toLeft = function(i) {
         $scope.allProduce.push($scope.userVeggies[i]);
         $scope.userVeggies.splice(i, 1);
+        checkIfOver();
     };
 
     $scope.checkIfInArray = function(produce, array) {
-        var isPresent;
 
+        var isPresent;
         if (array.indexOf(produce) === -1) {
             isPresent = false;
         } else {
             isPresent = true;
         }
         return isPresent;
+
+    };
+
+    $scope.gameDone = function() {
+        var wonGame = true;
+        $scope.userFruits.forEach(function(fruit) {
+            if (!$scope.fruits.includes(fruit)) {
+                wonGame = false;
+                console.log(fruit);
+            }
+        });
+        $scope.userVeggies.forEach(function(veggie) {
+            if (!$scope.veggies.includes(veggie)) {
+                wonGame = false;
+                console.log(veggie);
+            }
+        });
+        return wonGame;
+    };
+
+    var checkIfOver = function() {
+        if ($scope.allProduce.length === 0) {
+            console.log('done');
+            $scope.gameEnd = true;
+            $scope.gameWon = $scope.gameDone();
+            if ($scope.gameWon) {
+                $scope.message = 'you won!!';
+            } else {
+                $scope.message = 'you lost! recheck your fruits and veggies';
+            }
+        }
     };
 }]);
 
